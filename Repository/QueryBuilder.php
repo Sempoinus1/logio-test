02a4c8a82a02c0cfc272e84a08d81ae76f5ba9e1
Sempoinus1 <lausmandavid1@gmail.com>
2021-06-22 12:42:51 +0200

    querybuilder select

--- Repository/QueryBuilder.php
+++ Repository/QueryBuilder.php
@@ -4,20 +4,49 @@ require($_SERVER['DOCUMENT_ROOT'] . '/config/db.php');
 
 class QueryBuilder
 {
-    private $conn;
-
-    /**
-     * QueryBuilder constructor.
-     * @param $conn
-     */
-    public function __construct($conn)
-    {
-        $this->conn = $conn;
-    }
+    private $fields = [];
+    private $condition = [];
+    private $from = [];
 
     public function createConn()
     {
-        $this->conn = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_DATABASE, DB_USER, DB_PASS);
+        try {
+            return new PDO('mysql:host=' . DB_SERVER . ';dbname=' . DB_DATABASE, DB_USER, DB_PASS);
+        }
+        catch (PDOException $e){
+            print "Can't connect: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+
+    public function select(string ...$select): self
+    {
+        $this->fields = $select;
+        return $this;
+    }
+
+    public function where(string ...$where): self
+    {
+        foreach($where as $arg)
+            $this->condition = $arg;
+        return $this;
+    }
+
+    public function from(string $table, ?string $alias = null): self
+    {
+        if($alias == null)
+            $this->from[] = $table;
+        else
+            $this->from[] = "${table} AS ${alias}";
+        return $this;
+    }
+
+    public function __build(): string
+    {
+        $where = $this->condition === [] ? '' : ' WHERE' . implode(' AND ', $this->condition);
+        return 'SELECT ' . implode(', ', $this->fields)
+            . ' FROM ' . implode(', ', $this->from)
+            . $where;
     }
 
 }
